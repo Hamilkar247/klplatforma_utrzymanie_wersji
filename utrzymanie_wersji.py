a028604d3cd78447499ba1e94d4ba8ea6134eab2
Hamilkar247 <adonnibal96@gmail.com>
2022-08-15 17:44:54 +0200

    [WO-60] teraz jednak z tą linią jest więcej problemu niż pożytku - usuwam
    Signed-off-by: Hamilkar247 <adonnibal96@gmail.com>

--- utrzymanie_wersji.py
+++ utrzymanie_wersji.py
@@ -160,7 +160,6 @@ def main():
         while True:
             if os.name == "posix":
                 fp.drukuj("posix")
-                basic_path_ram=fp.zmienna_env_folder("basic_path_ram", ".env_projektu - problem z basic_path_ram")
                 basic_path_klplatforma_odbior_wysylka=os.getenv("basic_path_klplatforma_odbior_wysylka")
                 head, tail = os.path.split(basic_path_klplatforma_odbior_wysylka)
                 if os.path.isdir(basic_path_ram) == True:
