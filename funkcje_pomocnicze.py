533faf202f6aa895b6eef7724430199688c4a8e9
Hamilkar247 <adonnibal96@gmail.com>
2022-08-21 15:52:26 +0200

    [WO-96] durny błąð - nadpisywałem pid wartością zero
    Signed-off-by: Hamilkar247 <adonnibal96@gmail.com>

--- funkcje_pomocnicze.py
+++ funkcje_pomocnicze.py
@@ -97,7 +97,6 @@ class FunkcjePomocnicze():
         flara_file.close()
     
     def sprawdz_czy_program_o_tym_pid_dziala(self, pid):
-        pid = 0
         if psutil.pid_exists(pid):
             self.drukuj("a process with pid %d exists" % pid)
             return
